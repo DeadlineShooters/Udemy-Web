bb64c34ab76585fbaf6372f2e4d6d47297b403b2
nmthong226 <nmthong226@gmail.com>
2024-02-23 23:22:06 +0700

    code base

---
+++ udemy/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center font-bold mt-10 text-green-700'>
+        Hello everybody! I'm React & TailwindCSS component
+      </h1>
+    </>
+  );
+}
+
+export default App;
