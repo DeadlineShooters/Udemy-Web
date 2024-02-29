f034dff6d7941d6ea3a6bfa695a514907a1f2d93
nmthong226 <nmthong226@gmail.com>
2024-02-29 22:20:28 +0700

    + top navigation

--- udemy-front-end/src/App.js
+++ udemy-front-end/src/App.js
@@ -1,13 +1,46 @@
-import logo from './logo.svg';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Pages/home.jsx';
+import Register from "./Components/Pages/register.jsx";
+import Login from "./Components/Pages/login.jsx";
+import {createBrowserRouter, RouterProvider, Routes, Route, Outlet} from 'react-router-dom'
 import './App.css';
+
+const Layout = () => {
+  return(
+    <>
+      <Navbar/>
+      <Outlet/>
+      {/* <Footer/>*/}
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {  
+      path: "/",
+        element: <Home/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
 
 function App() {
   return (
-    <>
-      <h1 className='text-center font-bold mt-10 text-green-700'>
-        Hello everybody! I'm React & TailwindCSS component
-      </h1>
-    </>
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
   );
 }
 
---
+++ udemy-front-end/src/Components/Pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      This is home page
+    </div>
+  )
+}
+
+export default Home
