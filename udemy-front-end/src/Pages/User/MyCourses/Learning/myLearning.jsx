a31b805c54838e717de82abf67379dbe39a940a9
nmthong226 <nmthong226@gmail.com>
2024-03-03 22:12:00 +0700

    +view enrolled course

--- udemy-front-end/src/Pages/User/MyCourses/Learning/myLearning.jsx
+++ udemy-front-end/src/Pages/User/MyCourses/Learning/myLearning.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Link, useLocation } from "react-router-dom";
 import './myLearning.css';
+import course_placeholder1 from "../../../../Assets/Course.jpg"
+import course_placeholder2 from "../../../../Assets/Course2.png"
+import course_placeholder3 from "../../../../Assets/Course3.jpg"
 
 const MyLearning = () => {
+  const courses = [{
+    title: "Software design for beginners (2024)",
+    img: course_placeholder1,
+    instructor: "FIT HCMUS",
+    progress: 1/8,
+  }, {
+    title: "Introduction to Python (newest version, 2024)",
+    img: course_placeholder2,
+    instructor: "Tomato Group",
+    progress: 1/7,
+  }, {
+    title: "Data visualization with Kibana",
+    img: course_placeholder3,
+    instructor: "Onee Academy",
+    progress: 0,
+  }]
   return (
     <div>
       <div className="upper">
-          <h1 class="text-5xl font-bold pl-96 pt-10 pb-10">My learning</h1>
-          <div className='flex items-center pl-96'>
+          <h1 className="title text-5xl font-bold pt-10 pb-10">My learning</h1>
+          <div className='filter flex items-center'>
             <button class="text-white hover:bg-violet-950 border-b-8 font-bold py-2 rounded text-lg">
               <Link to="/home/my-courses/learning">All courses</Link>
             </button>
@@ -20,7 +39,7 @@ const MyLearning = () => {
           </div>
       </div>
       <div className="lower">
-        <form class="items-end w-1/4 mr-72 ml-auto">
+        <form className="searchbar items-end w-1/4 ml-auto">
           <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
           <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -32,7 +51,27 @@ const MyLearning = () => {
             <button type="submit" class="text-white absolute end-1 bottom-1 bg-violet-950 hover:bg-violet-900 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
           </div>
         </form>
-        <h1>This is my all courses page</h1>
+        <div class='flex justify-center min-h-screen mx-auto mt-5'>
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:p-72 lg:pt-0">
+            {courses.map((course)=> (
+              <div class='card'>
+                <div class='rounded-xl shadow-lg min-h-96'>
+                  <div class='p-3 flex flex-col'>
+                    <div class='rounded-xl overflow-hidden'>
+                      <img class='object-fit lg:h-48' src={course.img} alt='course placeholder' />
+                    </div>
+                    <h5 class='text-lg md:text-lg font-bold mt-2'>{course.title}</h5>
+                    <p class='text-slate-500 text-base mt-3'>{course.instructor}</p>
+                    <div class="w-full bg-gray-200 rounded-full h-2 mt-3 dark:bg-gray-700">
+                      <div class="bg-blue-600 h-2 rounded-full" style={{width: (course.progress * 100) + "%"}}></div>
+                    </div>
+                    <p class='text-slate-500 text-base mt-3'>{(course.progress.toPrecision(4) * 100)}% Complete</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
