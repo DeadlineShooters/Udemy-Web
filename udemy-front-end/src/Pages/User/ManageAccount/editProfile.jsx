20c32716c10fbdcca4facda165f23f153ff1aac9
nmthong226 <nmthong226@gmail.com>
2024-03-05 17:57:49 +0700

    fix bug change value

--- udemy-front-end/src/Pages/User/ManageAccount/editProfile.jsx
+++ udemy-front-end/src/Pages/User/ManageAccount/editProfile.jsx
@@ -7,7 +7,7 @@ const EditProfile = () => {
   const user = {
     firstName: "Nguyen Minh",
     lastName: "Thong",
-    heading: "Software engineering",
+    heading: "Software engineer",
     description: "",
     website: {
       fb: "",
@@ -21,10 +21,10 @@ const EditProfile = () => {
     <div class="template flex flex-row pt-8">
       <div class="flex flex-col border border-r-0 border-slate-500" style={{height: "40rem"}}>
         <div class='p-10'>
-          <div class="relative flex rounded-full">
+          <div class="flex rounded-full justify-center">
             <img id="preview" src={createImageFromInitials(160, fullname, getColor())} alt="profile-pic" className="avatar"/>
           </div>
-          <p class="text-xl mt-3 font-bold text-center">{user.firstName} {user.lastName}</p>
+          <p class="text-xl mt-3 font-bold text-center">{fullname}</p>
           <p class="text-lg italic text-center text-gray-500">{user.heading}</p>
         </div>
         <div class="pl-10 py-2 border border-l-0 border-r-0 border-slate-500 bg-sky-950 text-white font-bold">
@@ -46,32 +46,32 @@ const EditProfile = () => {
           <div class='flex flex-col'>
             <p class='font-bold px-12 pt-2'>Basics:</p>
             <form class='w-full px-12 pt-2'>
-              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={user.firstName}/>
+              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={user.firstName}/>
             </form>
             <form class='w-full px-12 pt-2'>
-              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={user.lastName}/>
+              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={user.lastName}/>
             </form>
             <form class='w-full px-12 pt-2'>
-              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={user.heading}/>
+              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={user.heading}/>
             </form>
           </div>
           <div class='flex flex-col'>
             <p class='font-bold px-12 pt-2'>Description:</p>
             <form class="px-12 pt-2">
-              <textarea id="message" rows="4" class="block p-1 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-500 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" value={user.description}/>
+              <textarea id="message" rows="4" class="block p-1 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-500 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" defaultValue={user.description}/>
             </form>
           </div>
           <div class='flex flex-col'>
             <p class='font-bold px-12 pt-2'>Links:</p>
             <form class='w-full px-12 pt-2'>
-              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Website (http(s)://..)' value={user.website.link}/>
+              <input type="text" id="small-input" class="block w-full p-2 text-gray-900 border border-gray-500 rounded-lg bg-gray-50 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder='Website (http(s)://..)' defaultValue={user.website.link}/>
             </form>
             <form class='w-full px-12 pt-2'>
               <div class="flex">
                 <span class="inline-flex items-center text-left pl-3 pr-2 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-500 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                   <p>http://www.facebook.com/</p>
                 </span>
-                <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Facebook Profile" value={user.website.fb}/>
+                <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Facebook Profile" defaultValue={user.website.fb}/>
               </div>
             </form>
             <form class='w-full px-12 pt-2'>
@@ -79,7 +79,7 @@ const EditProfile = () => {
                 <span class="inline-flex items-center text-left pl-3 pr-3.5 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-500 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                   <p>http://www.youtube.com/</p>
                 </span>
-                <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Youtube Profile" value={user.website.yb}/>
+                <input type="text" id="website-admin" class="rounded-none rounded-e-lg bg-gray-50 border border-gray-500 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Youtube Profile" defaultValue={user.website.yb}/>
               </div>
             </form>
           </div>
