b30b94c6ab67c0367185a79b9fa8148635aa0623
ItsMeNozo <dungtuyetngoc@gmail.com>
2024-03-17 15:17:46 +0700

    finish feedback dashboard

--- frontend/src/Components/CourseFeedback/Review.jsx
+++ frontend/src/Components/CourseFeedback/Review.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import { createImageFromInitials } from "../Utils/Utils";
+import StarRatings from "../StarRatings";
+import { useState } from "react";
+import InstructorResponse from "./InstructorResponse";
 
 const Review = ({ review }) => {
+  const [showResponse, setShowResponse] = useState(false);
+  const handleSeeResponseClick = () => {
+    setShowResponse((prevShowResponse) => !prevShowResponse);
+  };
+
   return (
-    <div className="flex p-4 bg-white shadow rounded-lg">
-      <div className="flex flex-col flex-grow mr-4">
-        <div className="flex items-center mb-2">
-          <img src={createImageFromInitials(160, review.firstName + " " + review.lastName)} alt="avatar" className="w-10 h-10 rounded-full mr-2" />
-          <div>
-            <div className="font-bold">{`${review.firstName} ${review.lastName}`}</div>
-            <div className="text-sm text-gray-500">{review.date}</div>
+    <div className="flex p-4 bg-white shadow rounded-lg items-start justify-between mb-2 ">
+      <div className="flex w-3/4 ">
+        <img src={createImageFromInitials(160, review.firstName + " " + review.lastName)} alt="avatar" className="w-10 h-10 rounded-full mr-2" />
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <div className="font-bold text-gray-500 text-sm mr-1">{`${review.firstName} ${review.lastName}`}</div>
+            <div className="text-sm text-gray-500">• {review.date}</div>
           </div>
+          <StarRatings rating={review.rating} />
+          <div className="mb-2">{review.feedback}</div>
+          {review.instructorResponse ? (
+            <button onClick={handleSeeResponseClick} className=" text-gray-500 text-sm font-bold rounded text-start">
+              {showResponse ? "HIDE RESPONSE" : "SEE RESPONSE"}
+            </button>
+          ) : (
+            <button className=" text-gray-500 text-sm font-bold rounded text-start">RESPOND</button>
+          )}
+
+          <div className="show-response">{review.instructorResponse && showResponse && <InstructorResponse response={review.instructorResponse} />}</div>
         </div>
-        <div className="mb-2">
-          <div>{review.feedback}</div>
-        </div>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded">Respond</button>
       </div>
-      <div className="flex flex-col items-center">
-        <img src={review.courseThumbnail} alt="course thumbnail" className="w-20 h-20 rounded-lg mb-2" />
-        <div className="font-bold">{review.courseName}</div>
+
+      <div className="flex flex-row justify-end">
+        <img src={review.courseThumbnail} alt="course thumbnail" className="flex-1 w-10 h-auto rounded-lg mr-2 " />
+        <span className="text-sm text-balance inline-block flex-1">{review.courseName}</span>
       </div>
     </div>
   );
--- frontend/src/Pages/Instructor/Reviews.jsx
+++ frontend/src/Pages/Instructor/Reviews.jsx
@@ -12,8 +12,15 @@ const reviews = [
     avatar: "https://via.placeholder.com/150",
     rating: 5,
     feedback: "Great course!",
-    courseName: "React.js Course",
-    courseThumbnail: "https://via.placeholder.com/150",
+    courseName: "The Complete 2024 Software Testing Bootcamp ",
+    courseThumbnail:
+      "https://cdn.discordapp.com/attachments/973498508793503745/1217696152044961852/2151486_095a_6.jpg?ex=6604f6ea&is=65f281ea&hm=2c883878c56eeeccff8c64f989924f67a38678a3e4254290d7c09392b68b6d86&",
+    instructorResponse: {
+      firstName: "Ngoc",
+      lastName: "Pham",
+      content: "Thank you",
+      createdTime: "2024-03-18",
+    },
   },
   {
     id: "1123",
@@ -21,10 +28,11 @@ const reviews = [
     lastName: "Doe",
     date: "2024-03-17",
     avatar: "https://via.placeholder.com/150",
-    rating: 5,
+    rating: 4,
     feedback: "Great course!",
-    courseName: "React.js Course",
-    courseThumbnail: "https://via.placeholder.com/150",
+    courseName: "The Complete 2024 Software Testing Bootcamp",
+    courseThumbnail:
+      "https://cdn.discordapp.com/attachments/973498508793503745/1217696152044961852/2151486_095a_6.jpg?ex=6604f6ea&is=65f281ea&hm=2c883878c56eeeccff8c64f989924f67a38678a3e4254290d7c09392b68b6d86&",
   },
   // Add more review objects here...
 ];
@@ -32,7 +40,7 @@ const reviews = [
 const Reviews = () => {
   return (
     <DashboardHeaderTitle title={"Reviews"}>
-      <div className="flex items-end  filter-container border-b border-gray-500 pb-3">
+      <div className="flex items-end  filter-container pb-3">
         <div className="flex items-center mr-8">
           <input type="checkbox" id="notResponded" className="mr-2" />
           <label htmlFor="notResponded">Not Responded</label>
@@ -55,14 +63,16 @@ const Reviews = () => {
           </select>
         </div>
       </div>
-      {reviews.length > 0 ? (
-        reviews.map((review) => {
-          // Render your review component here using 'review' data
-          return <Review key={review.id} review={review} />;
-        })
-      ) : (
-        <div className="flex justify-center mt-3">No reviews found</div>
-      )}
+      <div className="flex flex-grow flex-col justify-center mt-3 ">
+        {reviews.length > 0 ? (
+          reviews.map((review) => {
+            // Render your review component here using 'review' data
+            return <Review key={review.id} review={review} />;
+          })
+        ) : (
+          <span>No reviews found</span>
+        )}
+      </div>
     </DashboardHeaderTitle>
   );
 };
