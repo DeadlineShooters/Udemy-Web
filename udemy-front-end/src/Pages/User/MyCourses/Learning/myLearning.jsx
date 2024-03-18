61ffdde53f130015e4315d0c3f03cf5d96e14699
nmthong226 <nmthong226@gmail.com>
2024-03-18 22:57:37 +0700

    Fix course list

--- udemy-front-end/src/Pages/User/MyCourses/Learning/myLearning.jsx
+++ udemy-front-end/src/Pages/User/MyCourses/Learning/myLearning.jsx
@@ -31,108 +31,106 @@ const MyLearning = () => {
     img: course_placeholder3,
     instructor: "Onee Academy",
     progress: 0,
-  },]
+  }, ]
   return (
     <div>
       <div className="upper">
           <h1 className="title text-5xl max-sm:text-4xl font-bold pt-10 pb-10">My learning</h1>
           <div className='filter flex items-center'>
-            <button class="text-white border-b-8 font-bold py-2 rounded text-lg">
+            <button className="text-white border-b-8 font-bold py-2 rounded text-lg">
               <Link to="/home/my-courses/learning">All courses</Link>
             </button>
-            <button class="text-whit lg:border-b-8 border-slate-900 font-bold py-2 rounded text-lg mx-8">
+            <button className="text-whit lg:border-b-8 border-slate-900 font-bold py-2 rounded text-lg mx-8">
               <Link to="/home/my-courses/wishlist">Wishlist</Link>
             </button>
-            <button class="text-white lg:border-b-8 border-slate-900 font-bold py-2 rounded text-lg">
+            <button className="text-white lg:border-b-8 border-slate-900 font-bold py-2 rounded text-lg">
               <Link to="/home/my-courses/archived">Archived</Link>
             </button>
           </div>
       </div>
       <div className="lower">
         <form className="searchbar items-end w-1/4 ml-auto">
-          <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-          <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
             </div>
-            <input type="search" id="default-search" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search my course..." required />
-            <button type="submit" class="text-white absolute end-1 bottom-1 bg-violet-950 hover:bg-violet-900 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search my course..." required />
+            <button type="submit" className="text-white absolute end-1 bottom-1 bg-violet-950 hover:bg-violet-900 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
           </div>
         </form>
-        <div class='flex justify-center min-h-screen mx-auto mt-5'>
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {courses.map((course)=> (
-              <div class='card flex flex-col h-full'>
-                <div class='rounded-xl shadow-lg flex-grow'>
-                  <div class='p-3 flex flex-col h-full justify-between'>
-                    <div class='rounded-xl overflow-hidden justify-between relative'>
-                      <div class='rounded-xl overflow-hidden'>
-                        <img class='object-fit h-80 w-auto lg:h-48 lg:w-auto' src={course.img} alt='course placeholder' />
-                      </div>
-                      <div class='rounded-xl overflow-hidden absolute top-0 left-0 opacity-0 hover:opacity-100'>
-                        <a href='/course/course-name/learn/video-id'>
-                          <img class='object-fit h-80 w-full lg:h-48 lg:w-full' src={course_overlay} alt='course placeholder' />
-                        </a>
-                      </div>
-                      <div class='rounded-md absolute top-0 right-0 m-2 w-12 h-12'>
-                        <Menu as="div" className="relative ml-4">
-                          <div>
-                            <Menu.Button className="relative flex rounded-full bg-white-800 text-sm focus:ring-2">
-                              <img id="preview" src={more_actions} alt="profile-pic" className="avatar"/>
-                            </Menu.Button>
-                          </div>
-                            <Transition
-                              as={Fragment}
-                              enter="transition ease-out duration-100"
-                              enterFrom="transform opacity-0 scale-95"
-                              enterTo="transform opacity-100 scale-100"
-                              leave="transition ease-in duration-75"
-                              leaveFrom="transform opacity-100 scale-100"
-                              leaveTo="transform opacity-0 scale-95"
-                            >
-                              <Menu.Items className="absolute right-0 z-9999 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg">
-                                <Menu.Item>
-                                  {({ active }) => (
-                                    <div className={classNames(active ? 'bg-gray-100' : '', 'flex flex-row items-center px-4 py-2 text-sm text-gray-700')}>
-                                      <img src={share} alt='share' class='h-5 w-5 mr-5'/>
-                                      <a href="/user/public-profile">
-                                        Share
-                                      </a>
-                                    </div>
-                                  )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                  {({ active }) => (
-                                    <div className={classNames(active ? 'bg-gray-100' : '', 'flex flex-row items-center px-4 py-2 text-sm text-gray-700')}>
-                                      <img src={archive} alt='share' class='h-5 w-5 mr-5'/>
-                                      <a href="/user/public-profile">
-                                        Archive
-                                      </a>
-                                    </div>
-                                  )}
-                                </Menu.Item>
-                              </Menu.Items>
-                          </Transition>
-                        </Menu>
-                      </div>
-                      <p class='text-lg md:text-lg font-bold mt-2'>{course.title}</p>
+        <div className='flex flex-wrap justify-center m-auto w-full mt-5'>
+          {courses.map((course, index)=> (
+            <div key={index} className='card flex flex-col'>
+              <div className='rounded-xl shadow-lg flex-grow'>
+                <div className='p-3 flex flex-col h-full justify-between'>
+                  <div className='rounded-xl overflow-hidden justify-between relative'>
+                    <div className='rounded-xl overflow-hidden'>
+                      <img className="thumbnail" src={course.img} alt='course placeholder' />
                     </div>
-                    <div>
-                      <p class='text-slate-500 text-base mt-3'>{course.instructor}</p>
-                      <div class="w-full bg-gray-200 rounded-full h-2 mt-3 dark:bg-gray-700">
-                        <div class="bg-blue-600 h-2 rounded-full" style={{width: (course.progress * 100) + "%"}}></div>
-                      </div>
-                      <p class='text-slate-500 text-base mt-3'>{(course.progress.toPrecision(4) * 100)}% Complete</p>
+                    <div className='rounded-xl overflow-hidden absolute top-0 left-0 opacity-0 hover:opacity-100'>
+                      <a href='/course/course-name/learn/video-id'>
+                        <img className='thumbnail-shadow' src={course_overlay} alt='course placeholder' />
+                      </a>
                     </div>
+                    <div className='rounded-md absolute top-0 right-0 mt-6 mr-2 w-12 h-12'>
+                      <Menu as="div" className="relative ml-4">
+                        <div>
+                          <Menu.Button className="relative flex rounded-full bg-white-800 text-sm focus:ring-2">
+                            <img id="preview" src={more_actions} alt="profile-pic" className="avatar"/>
+                          </Menu.Button>
+                        </div>
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                          >
+                            <Menu.Items className="absolute right-0 z-9999 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <div className={classNames(active ? 'bg-gray-100' : '', 'flex flex-row items-center px-4 py-2 text-sm text-gray-700')}>
+                                    <img src={share} alt='share' className='h-5 w-5 mr-5'/>
+                                    <a href="/user/public-profile">
+                                      Share
+                                    </a>
+                                  </div>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <div className={classNames(active ? 'bg-gray-100' : '', 'flex flex-row items-center px-4 py-2 text-sm text-gray-700')}>
+                                    <img src={archive} alt='share' className='h-5 w-5 mr-5'/>
+                                    <a href="/user/public-profile">
+                                      Archive
+                                    </a>
+                                  </div>
+                                )}
+                              </Menu.Item>
+                            </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                    <p className='text-lg md:text-lg font-bold mt-2'>{course.title}</p>
+                  </div>
+                  <div>
+                    <p className='text-slate-500 text-base mt-3'>{course.instructor}</p>
+                    <div className="w-full bg-gray-200 rounded-full h-2 mt-3 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2 rounded-full" style={{width: (course.progress * 100) + "%"}}></div>
+                    </div>
+                    <p className='text-slate-500 text-base mt-3'>{(course.progress.toPrecision(4) * 100)}% Complete</p>
                   </div>
                 </div>
               </div>
+            </div>
             ))}
           </div>
         </div>
-      </div>
     </div>
   )
 }
