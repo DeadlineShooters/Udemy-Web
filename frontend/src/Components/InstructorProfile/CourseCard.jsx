58bc6f2e771fcc7adbdfddcda3eaaf507cf37c0b
999Kit <98326962+999Kit@users.noreply.github.com>
2024-03-24 14:17:00 +0700

    finalized styles for course cards, fixed the rating stars

--- frontend/src/Components/InstructorProfile/CourseCard.jsx
+++ frontend/src/Components/InstructorProfile/CourseCard.jsx
@@ -1,4 +1,5 @@
-import { Rating } from "@material-tailwind/react";
+import React from 'react';
+import RatingStars from './RatingStars';
 
 
 const courseImage =
@@ -10,14 +11,13 @@ const CourseCard = (instructorInfo) => {
             <img src={courseImage} alt="" />
             <p className="font-bold"><span>[NEW] </span>Ultimate AWS Certified Cloud Practitioner CLF-C02</p>
             <p className="text-xs text-neutral-500">Stephane Maarek | AWS Certified Cloud...</p>
-            <div>4.7
-                <div style={{ fontSize: '2px' }}> {/* Applied custom CSS for smaller font size */}
-                    <Rating value={4} />
-                </div>
+            <div className='flex items-center gap-2'>
+                <span className='font-bold'>4.7</span>
+                <RatingStars rating={3}/>
                 <span className="text-xs text-neutral-500">(197,359)</span>
             </div>
             <div className="text-xs text-neutral-500	">15 total hours - 281 lectures - Beginner</div>
-            <div className="font-bold">đ399,000 đ2,199,000</div>
+            <div className="font-bold my-1">đ399,000 đ2,199,000</div>
             <div className="inline text-xs px-2 py-1 bg-amber-200 font-bold">Bestseller</div>
         </div>
     );
