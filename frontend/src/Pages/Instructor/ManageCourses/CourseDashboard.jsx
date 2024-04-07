6eb4678b8194e91b1f308fd757e00a3500a8c028
999Kit <98326962+999Kit@users.noreply.github.com>
2024-04-07 16:39:32 +0700

    added buttons and course cards

--- frontend/src/Pages/Instructor/ManageCourses/CourseDashboard.jsx
+++ frontend/src/Pages/Instructor/ManageCourses/CourseDashboard.jsx
@@ -1,13 +1,50 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import DashboardHeaderTitle from "../../../Components/DashboardHeaderTitle";
 import CreateCourseCard from "../../../Components/CourseDashboard/CreateCourseCard";
 import ResourceCard from "../../../Components/CourseDashboard/ResourceCard";
+import { Button } from "@material-tailwind/react";
+import { IoMdSearch } from "react-icons/io";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+
+
 const profileImage =
 	"https://res.cloudinary.com/dk6q93ryt/image/upload/v1696217092/samples/smile.jpg";
 
 
 const CourseDashBoard = () => {
-  const haveCourse = false;
+
+  const [selectedFilter, setSelectedFilter] = useState('');
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const res = [
+      {
+        name: "Learn ReactJS",
+        status: "DRAFT",
+        thumbnail: profileImage
+      },
+      {
+        name: "Learn ReactJS",
+        status: "DRAFT",
+        thumbnail: profileImage
+      },
+      {
+        name: "Learn ReactJS",
+        status: "DRAFT",
+        thumbnail: profileImage
+      },
+    ]
+
+    setCourses(res);
+  }, [])
+
+  const handleFilterSelect = (filter) => {
+    setSelectedFilter(filter);
+    // Perform filtering logic based on selected filter
+    // alert(filter)
+  };
+  const haveCourse = true;
 
   const resource1 = {
     imgSrc: profileImage,
@@ -38,29 +75,54 @@ const CourseDashBoard = () => {
     <DashboardHeaderTitle title={"Courses"}>
       {/* <a href="/instructor/course/123/manage/curriculum">Edit course</a> */}
       {haveCourse && (
-      <div className="bg-gray-100 min-h-screen">
-        <div className="container mx-auto py-8">
-          <h1 className="text-3xl font-bold mb-4">My Courses</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            <CourseCard
-              title="Course 1"
-              instructor="John Doe"
-              imageUrl="https://via.placeholder.com/300"
-              rating={4.5}
-            />
-            <CourseCard
-              title="Course 2"
-              instructor="Jane Smith"
-              imageUrl="https://via.placeholder.com/300"
-              rating={4.8}
-            />
+        <div>
+          <div className="mt-2 flex justify-between">
+            <div className="flex gap-10">
+              <div className="flex">
+                <input className="px-4 border border-black" type="text" placeholder="Search your courses" style={{widows: "186px", height: "48px"}} />
+                <button className="w-12 h-12 bg-black flex items-center justify-center"><IoMdSearch className="w-6 h-6 text-white" /></button>
+              </div>
+          
+              {/* <Button color="white" className="rounded-none hover:bg-gray-300 border border-black shadow-none" style={{height: "48px"}}>
+                <span className="font-bold text-base normal-case">Newest</span>
+              </Button> */}
+              <DropdownMenu onSelect={handleFilterSelect} />
+            </div>
+            <Button color="purple" className="rounded-none hover:bg-violet-800" style={{height: "48px"}}>
+              <span className="font-bold text-base normal-case">New Course</span>
+            </Button>
           </div>
-        </div>
+
+
+          <div className="my-courses mt-6">
+            {courses.map((course, index) => (
+              <div className="flex mb-6" key={index}>
+
+                <img src={course.thumbnail} className="object-cover" style={{width: "118px", height: "118px"}} />
+                <div className="flex-1 border border-gray-400 p-2 flex relative">
+                  <div className="content-block w-1/4 flex flex-col justify-between">
+                    <p className="font-bold text-sm">{course.name}</p>
+                    <p className="font-bold text-xs">{course.status}</p>
+                  </div>
+                  <div className="w-3/4 flex items-center">
+                    <p className="flex-none text-xs mr-2 font-bold">Finish your course</p>
+                    <div class=" flex-1 bg-gray-300 h-2  overflow-hidden">
+                      <div class="bg-blue-500 h-full w-1/2 "></div>
+                    </div>
+                  </div>
+
+                  <button className="edit-cover absolute top-0 left-0 w-full h-full bg-white opacity-0 hover:opacity-90 flex items-center justify-center hover:cursor-pointer" onClick={() => alert('aa')}>
+                    <p className="font-bold text-purple-500">Edit / manage course</p>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
 
         </div>
       )}
       
-      <CreateCourseCard />
+      {!haveCourse && (<CreateCourseCard />)}
       
       <h2 className="mx-auto my-16 text-center">Based on your experience, we think these resources will be helpful.</h2>
 
@@ -80,18 +142,40 @@ const CourseDashBoard = () => {
 
 export default CourseDashBoard;
 
-function CourseCard({ title, instructor, imageUrl, rating }) {
+const DropdownMenu = ({ onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [sortValue, setSortValue] = useState("Newest");
+
+  const handleSelect = (option) => {
+    setSortValue(option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  const optionStyle = "cursor-pointer py-2 px-4 m-0 text-sm hover:text-purple-500 w-full text-left";
+
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <img src={imageUrl} alt={title} className="w-full h-40 object-cover mb-4 rounded-md" />
-      <h2 className="text-lg font-bold">{title}</h2>
-      <p className="text-gray-600 mb-2">{instructor}</p>
-      <div className="flex items-center">
-        <svg className="w-5 h-5 text-yellow-500 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path fillRule="evenodd" d="M10 1l2.39 6.49h6.18l-4.99 3.93L15.5 19 10 15.31 4.51 19l1.4-6.58L3.43 7.49h6.18L10 1zm0 2.13L8.72 7.49H3.72l4.06 3.2-1.54 7.26L10 14.69l4.76 3.26-1.54-7.26 4.06-3.2h-5l-1.29-4.36z" clipRule="evenodd" />
-        </svg>
-        <span className="text-gray-600">{rating}</span>
-      </div>
+    <div className="relative">
+      <Button
+        color="white"
+        className="rounded-none hover:bg-gray-300 border border-black shadow-none flex px-3"
+        style={{ height: '48px' }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="font-bold text-base normal-case">{sortValue} </span>
+        <MdOutlineKeyboardArrowDown className="w-6 h-6" />
+      </Button>
+      {isOpen && (
+        <div className="z-9999 py-2 absolute top-14 left-0 w-48 bg-white border border-gray-300 shadow-md">
+            <button onClick={() => handleSelect('Newest')} className={optionStyle}>
+              Newest
+            </button>
+            <button onClick={() => handleSelect('Oldest')} className={optionStyle}>
+              Oldest
+            </button>
+        </div>
+      )}
     </div>
   );
-}
+};
+
