28cd5344c82005fe8983a6ba145e1d1e05706c76
nmthong226 <nmthong226@gmail.com>
2024-04-15 20:32:08 +0700

    Fix UI my learning

--- frontend/src/Pages/User/MyCourses/Learning/myLearning.jsx
+++ frontend/src/Pages/User/MyCourses/Learning/myLearning.jsx
@@ -69,25 +69,37 @@ const MyLearning = () => {
           </div>
       </div>
       <div className="lower-mylearning">
-        <form className="searchbar flex flex-row justify-center items-end mt-3 mx-5">
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-          <div className="filter flex flex-col filter-container mr-8">
-            <span>Filter by</span>
+        <div className="flex flex-row ml-auto justify-end items-center">
+          <div className="flex flex-row items-center px-5">
+            <span className='mr-2'>Filter by:</span>
             <select className="p-2 text-md hover:bg-gray-200 border border-gray-400 rounded-lg">
               <option value="all">All Courses</option>
               <option value="favorites">Favorites</option>
             </select>
           </div>
-          <div className="formsearch relative w-80">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-              </svg>
+          <form className="flex flex-row items-end lg:w-1/4 md:w-1/3 px-2">
+            <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
+            <div class="relative flex items-center">
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-2 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search my course..."
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute end-1 bg-purple-900 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                <svg class="w-4 h-4 text-white dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                </svg>
+              </button>
             </div>
-            <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search my course..." required />
-            <button type="submit" className="text-white absolute end-1 bottom-1 bg-purple-900 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-          </div>
-        </form>
+          </form>
+        </div>
         <div className='flex flex-wrap justify-center m-auto w-full'>
           {courses.map((course, index)=> (
             <div key={index} className='card flex flex-col'>
