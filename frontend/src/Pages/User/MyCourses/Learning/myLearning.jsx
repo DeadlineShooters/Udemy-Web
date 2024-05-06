03a79295a572a04a3df74727fa077e08786f3cc4
nmthong226 <nmthong226@gmail.com>
2024-05-06 21:41:46 +0700

    spinner

--- frontend/src/Pages/User/MyCourses/Learning/myLearning.jsx
+++ frontend/src/Pages/User/MyCourses/Learning/myLearning.jsx
@@ -217,22 +217,14 @@ const MyLearning = () => {
               <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
                 Search
               </label>
-              <div class="relative flex items-center">
+              <div class='relative flex items-center'>
                 <input
-                  type="search"
-                  id="default-search"
-                  class="block w-full p-2 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Search my course..."
+                  type='search'
+                  id='default-search'
+                  class='block w-full p-2 ps-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='Search my course...'
                   required
                 />
-                <button
-                  type="submit"
-                  class="text-white absolute end-1 bg-purple-900 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  <svg class="w-4 h-4 text-white dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                  </svg>
-                </button>
               </div>
             </form>
             <div className='flex flex-wrap justify-center md:justify-start'>
